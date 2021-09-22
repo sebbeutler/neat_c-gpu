922635e8815e8e7c153f15e0c9de9958625c7e4f
subski <58097205+subski@users.noreply.github.com>
2021-09-22 22:29:06 +0200

    Testing 2D arrays additions.

--- kernels/vector_add_kernel.cl
+++ kernels/vector_add_kernel.cl
@@ -1,8 +1,10 @@
-__kernel void vector_add(__global const int *A, __global const int *B, __global int *C) {
+__kernel
+void vector_add(__global const int *A, __global const int *B, __global int *C, int w) {
  
     // Get the index of the current element to be processed
     int i = get_global_id(0);
+    int j = get_global_id(1);
  
     // Do the operation
-    C[i] = A[i] + B[i];
+    C[i*w+j] = A[i*w+j] + B[i*w+j];
 }
--- main.c
+++ main.c
@@ -6,116 +6,111 @@
 
 #define MAX_SOURCE_SIZE (0x100000)
 
-int main(int argc, char* argv[]) {
-    // Create the two input vectors
-    int i;
-    const int LIST_SIZE = 1024;
-    int *A = (int*)malloc(sizeof(int)*LIST_SIZE);
-    int *B = (int*)malloc(sizeof(int)*LIST_SIZE);
-    for(i = 0; i < LIST_SIZE; i++) {
-        A[i] = i;
-        B[i] = LIST_SIZE - i;
-    }
+char BIN_PATH[255];
 
-    // Load the kernel source code into the array source_str
+cl_program load_program(cl_context context, char* filename)
+{
     FILE *fp;
     char *source_str;
     size_t source_size;
 
-    char bin_path[255];
-    strcpy(bin_path, argv[0]);
-	for (size_t i=strlen(bin_path)-1; i>=0; i--)
-	{
-		if (bin_path[i] == '/' || bin_path[i] == '\\')
-		{
-			bin_path[i+1] = '\0';
-			break;
-		}
-	}
 
     char kernel_path[255];
-    strcpy(kernel_path, bin_path);
-    strcat(kernel_path, "vector_add_kernel.cl");
+    strcpy(kernel_path, BIN_PATH);
+    strcat(kernel_path, filename);
     fp = fopen(kernel_path, "r");
     if (!fp) {
         fprintf(stderr, "Failed to load kernel.\n");
         exit(1);
     }
-    source_str = (char*)malloc(MAX_SOURCE_SIZE);
-    source_size = fread( source_str, 1, MAX_SOURCE_SIZE, fp);
-    fclose( fp );
+    source_str = (char *)malloc(MAX_SOURCE_SIZE);
+    source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+    fclose(fp);
 
-    // Get platform and device information
+    return clCreateProgramWithSource(context, 1, (const char **)&source_str, (const size_t *)&source_size, NULL);
+}
+
+void print_mat(int *mat, int width, int height)
+{
+    for (int i = 0; i < width; i++)
+    {
+        for (int j = 0; j < height; j++)
+        {
+            printf("%d ", *(mat + (i * sizeof(int)) + j));
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+// https://www.eriksmistad.no/getting-started-with-opencl-and-gpu-computing/
+int main(int argc, char *argv[])
+{    
+    strcpy(BIN_PATH, argv[0]);
+    for (size_t i = strlen(BIN_PATH) - 1; i >= 0; i--)
+    {
+        if (BIN_PATH[i] == '/' || BIN_PATH[i] == '\\')
+        {
+            BIN_PATH[i + 1] = '\0';
+            break;
+        }
+    }
+
     cl_platform_id platform_id = NULL;
-    cl_device_id device_id = NULL;   
-    cl_uint ret_num_devices;
-    cl_uint ret_num_platforms;
-    cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
-    ret = clGetDeviceIDs( platform_id, CL_DEVICE_TYPE_DEFAULT, 1, 
-            &device_id, &ret_num_devices);
+    cl_device_id device_id = NULL;
+    clGetPlatformIDs(1, &platform_id, NULL);
+    clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1, &device_id, NULL);
+    cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, NULL);
+    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, NULL);
 
-    // Create an OpenCL context
-    cl_context context = clCreateContext( NULL, 1, &device_id, NULL, NULL, &ret);
+    const int MWIDTH = 10;
+    const int MHEIGHT = 10;
+    const size_t MSIZE = MWIDTH * MHEIGHT * sizeof(int);
+    int A[10][10];
+    int B[10][10];
+    int C[10][10];
+    for (int i = 0; i < MWIDTH; i++)
+    {
+        for (int j = 0; j < MHEIGHT; j++)
+        {
+            A[i][j] = 1;
+            B[i][j] = 2;
+        }
+    }
 
-    // Create a command queue
-    cl_command_queue command_queue = clCreateCommandQueue(context, device_id, 0, &ret);
+    cl_program program = load_program(context, "vector_add_kernel.cl");
+    clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+    cl_kernel kernel = clCreateKernel(program, "vector_add", NULL);
 
-    // Create memory buffers on the device for each vector 
-    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY, 
-            LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,
-            LIST_SIZE * sizeof(int), NULL, &ret);
-    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, 
-            LIST_SIZE * sizeof(int), NULL, &ret);
-
-    // Copy the lists A and B to their respective memory buffers
-    ret = clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0,
-            LIST_SIZE * sizeof(int), A, 0, NULL, NULL);
-    ret = clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, 
-            LIST_SIZE * sizeof(int), B, 0, NULL, NULL);
-
-    // Create a program from the kernel source
-    cl_program program = clCreateProgramWithSource(context, 1, 
-            (const char **)&source_str, (const size_t *)&source_size, &ret);
-
-    // Build the program
-    ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
-
-    // Create the OpenCL kernel
-    cl_kernel kernel = clCreateKernel(program, "vector_add", &ret);
-
-    // Set the arguments of the kernel
-    ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void *)&a_mem_obj);
-    ret = clSetKernelArg(kernel, 1, sizeof(cl_mem), (void *)&b_mem_obj);
-    ret = clSetKernelArg(kernel, 2, sizeof(cl_mem), (void *)&c_mem_obj);
+    cl_mem a_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,  MSIZE, NULL, NULL);
+    cl_mem b_mem_obj = clCreateBuffer(context, CL_MEM_READ_ONLY,  MSIZE, NULL, NULL);
+    cl_mem c_mem_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY, MSIZE, NULL, NULL);
+    clEnqueueWriteBuffer(command_queue, a_mem_obj, CL_TRUE, 0, MSIZE, A, 0, NULL, NULL);
+    clEnqueueWriteBuffer(command_queue, b_mem_obj, CL_TRUE, 0, MSIZE, B, 0, NULL, NULL);
     
-    // Execute the OpenCL kernel on the list
-    size_t global_item_size = LIST_SIZE; // Process the entire lists
-    size_t local_item_size = 64; // Divide work items into groups of 64
-    ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, 
-            &global_item_size, &local_item_size, 0, NULL, NULL);
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), &a_mem_obj);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), &b_mem_obj);
+    clSetKernelArg(kernel, 2, sizeof(cl_mem), &c_mem_obj);
+    clSetKernelArg(kernel, 3, sizeof(int), &MWIDTH);
 
-    // Read the memory buffer C on the device to the local variable C
-    int *C = (int*)malloc(sizeof(int)*LIST_SIZE);
-    ret = clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, 
-            LIST_SIZE * sizeof(int), C, 0, NULL, NULL);
+    size_t global_item_size[] = { MWIDTH, MHEIGHT };
+    size_t local_item_size[] = { 1, 1 };
+    clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global_item_size, local_item_size, 0, NULL, NULL);
+    clEnqueueReadBuffer(command_queue, c_mem_obj, CL_TRUE, 0, MSIZE, C, 0, NULL, NULL);
+    
+    print_mat(A, MWIDTH, MHEIGHT);
+    print_mat(B, MWIDTH, MHEIGHT);
+    print_mat(C, MWIDTH, MHEIGHT);
 
-    // Display the result to the screen
-    for(i = 0; i < LIST_SIZE; i++)
-        printf("%d + %d = %d\n", A[i], B[i], C[i]);
+    clReleaseMemObject(a_mem_obj);
+    clReleaseMemObject(c_mem_obj);
 
-    // Clean up
-    ret = clFlush(command_queue);
-    ret = clFinish(command_queue);
-    ret = clReleaseKernel(kernel);
-    ret = clReleaseProgram(program);
-    ret = clReleaseMemObject(a_mem_obj);
-    ret = clReleaseMemObject(b_mem_obj);
-    ret = clReleaseMemObject(c_mem_obj);
-    ret = clReleaseCommandQueue(command_queue);
-    ret = clReleaseContext(context);
-    free(A);
-    free(B);
-    free(C);
+    clFlush(command_queue);
+    clFinish(command_queue);
+    clReleaseCommandQueue(command_queue);    
+    clReleaseKernel(kernel);
+    clReleaseProgram(program);    
+    clReleaseContext(context);
+    
     return 0;
 }
